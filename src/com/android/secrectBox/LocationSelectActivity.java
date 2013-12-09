e51b0ec69c8e113f71eb14f8734e09894f69cc29
6174 <570171025@qq.com>
2013-12-09 16:47:40 +0800

    百度地图定位功能

--- src/com/android/secrectBox/LocationSelectActivity.java
+++ src/com/android/secrectBox/LocationSelectActivity.java
@@ -1,11 +1,22 @@
 package com.android.secrectBox;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.BMapManager;
 import com.baidu.mapapi.MKGeneralListener;
 import com.baidu.mapapi.map.*;
@@ -15,22 +26,30 @@ import com.baidu.platform.comapi.basestruct.GeoPoint;
  * Created by Administrator on 13-12-8.
  */
 public class LocationSelectActivity extends Activity{
+
+
+    //--common part
     private Common common;
     private String LOGTAG;
-    /**
-     *  MapView 是地图主控件
-     */
-    private MapView mMapView = null;
-    /**
-     *  用MapController完成地图控制
-     */
-    private MapController mMapController = null;
-    /**
-     *  MKMapViewListener 用于处理地图事件回调
-     */
+    public static final String strKey = "SogUkjo6I8Ujev7GSFDVi5Oy";
+
+    //--ui elements
+    Button requestLocationBtn;
+
+    //--map objects
+    MyLocationMapView mMapView = null;
     MKMapViewListener mMapListener = null;
     BMapManager mBMapManager = null;
-    public static final String strKey = "SogUkjo6I8Ujev7GSFDVi5Oy";
+    MapController mMapController = null;
+
+    // location objects
+    LocationClient mLocClient;
+    LocationData locData = null;
+    MyLocationListenner myListener;
+    locationOverlay myLocationOverlay = null;
+    boolean isRequest = false;
+    boolean isNeedLocationAtInitialTime = true;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,125 +59,266 @@ public class LocationSelectActivity extends Activity{
          * 并在地图地图模块销毁后销毁，只要还有地图模块在使用，BMapManager就不应该销毁
          */
         mBMapManager = new BMapManager(this);
-        mBMapManager.init(strKey, new MyGeneralListener());
-
+        mBMapManager.init(strKey, null);
         /**
          * 由于MapView在setContentView()中初始化,所以它需要在BMapManager初始化之后
          */
         setContentView(R.layout.location_select_content);
-        mMapView = (MapView)findViewById(R.id.bmapView);
-        /**
-         * 获取地图控制器
-         */
+        init();
+    }
+
+    private void init(){
+        initCommon();
+        initUIElements();
+        initMap();
+        initUIEvents();
+    }
+
+    private void initCommon(){
+        common = Common.getInstance();
+        LOGTAG = common.getTag();
+    }
+
+    private void initUIElements(){
+        requestLocationBtn = (Button)findViewById(R.id.RequestLocationBtn);
+        mMapView = (MyLocationMapView)findViewById(R.id.bmapView);
+    }
+
+    private void initUIEvents(){
+        requestLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(LOGTAG, "clicked request location btn");
+                requestLocation();
+            }
+        });
+    }
+
+    private void initMap(){
+        initMapController();
+//        initMapPos();
+        initMapListener();
+        initLocation();
+        requestLocation();
+    }
+
+    private void initMapController(){
         mMapController = mMapView.getController();
-        /**
-         *  设置地图是否响应点击事件  .
-         */
         mMapController.enableClick(true);
-        /**
-         * 设置地图缩放级别
-         */
-        mMapController.setZoom(12);
-
-        /**
-         * 将地图移动至指定点
-         * 使用百度经纬度坐标，可以通过http://api.map.baidu.com/lbsapi/getpoint/index.html查询地理坐标
-         * 如果需要在百度地图上显示使用其他坐标系统的位置，请发邮件至mapapi@baidu.com申请坐标转换接口
-         */
+        mMapController.setZoom(14);
+        mMapView.setBuiltInZoomControls(true);
+    }
+    /**
+     * 将地图移动至指定点
+     * 使用百度经纬度坐标，可以通过http://api.map.baidu.com/lbsapi/getpoint/index.html查询地理坐标
+     * 如果需要在百度地图上显示使用其他坐标系统的位置，请发邮件至mapapi@baidu.com申请坐标转换接口
+     */
+    private void initMapPos(){
         GeoPoint p ;
-        double cLat = 39.945 ;
-        double cLon = 116.404 ;
-        Intent intent = getIntent();
-        if ( intent.hasExtra("x") && intent.hasExtra("y") ){
-            //当用intent参数时，设置中心点为指定点
-            Bundle b = intent.getExtras();
-            p = new GeoPoint(b.getInt("y"), b.getInt("x"));
-        }else{
-            //设置中心点为天安门
-            p = new GeoPoint((int)(cLat * 1E6), (int)(cLon * 1E6));
-        }
-
+        double cLat = 115.403350;// 39.945 ;
+        double cLon = 39.044865; // ;
+//        locData.latitude = 115.403350;
+//        locData.longitude = 39.044865;
+        //设置中心点为天安门
+        p = new GeoPoint((int)(cLat * 1E6), (int)(cLon * 1E6));
         mMapController.setCenter(p);
+    }
 
+    private void initMapListener(){
         /**
          *  MapView的生命周期与Activity同步，当activity挂起时需调用MapView.onPause()
          */
         mMapListener = new MKMapViewListener() {
+            /**
+             * 在此处理地图移动完成回调
+             * 缩放，平移等操作完成后，此回调被触发
+             */
             @Override
             public void onMapMoveFinish() {
-                /**
-                 * 在此处理地图移动完成回调
-                 * 缩放，平移等操作完成后，此回调被触发
-                 */
             }
 
+            /**
+             * 在此处理底图poi点击事件
+             * 显示底图poi名称并移动至该点
+             * 设置过： mMapController.enableClick(true); 时，此回调才能被触发
+             *
+             */
             @Override
             public void onClickMapPoi(MapPoi mapPoiInfo) {
-                /**
-                 * 在此处理底图poi点击事件
-                 * 显示底图poi名称并移动至该点
-                 * 设置过： mMapController.enableClick(true); 时，此回调才能被触发
-                 *
-                 */
                 String title = "";
                 if (mapPoiInfo != null){
                     title = mapPoiInfo.strText;
-//                    Toast.makeText(BaseMapDemo.this,title,Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LocationSelectActivity.this,title,Toast.LENGTH_SHORT).show();
                     mMapController.animateTo(mapPoiInfo.geoPt);
                 }
             }
-
-            @Override
-            public void onGetCurrentMap(Bitmap b) {
-                /**
-                 *  当调用过 mMapView.getCurrentMap()后，此回调会被触发
-                 *  可在此保存截图至存储设备
-                 */
-            }
-
-            @Override
-            public void onMapAnimationFinish() {
-                /**
-                 *  地图完成带动画的操作（如: animationTo()）后，此回调被触发
-                 */
-            }
             /**
-             * 在此处理地图载完成事件
+             *  当调用过 mMapView.getCurrentMap()后触发
              */
             @Override
+            public void onGetCurrentMap(Bitmap b) {
+            }
+            @Override
+            public void onMapAnimationFinish() {
+            }
+            @Override
             public void onMapLoadFinish() {
-//                Toast.makeText(BaseMapDemo.this,
-//                        "地图加载完成",
-//                        Toast.LENGTH_SHORT).show();
-
             }
         };
         mMapView.regMapViewListener(mBMapManager, mMapListener);
     }
 
+    private void initLocation(){
+
+        Log.i(LOGTAG, "initLocation");
+        //定位初始化
+        mLocClient = new LocationClient(getApplicationContext());
+
+        myListener = new MyLocationListenner();
+        mLocClient.registerLocationListener(myListener);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);//打开gps
+        option.setCoorType("bd09ll");     //设置坐标类型
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+        mLocClient.requestLocation();
+
+        //定位图层初始化
+        myLocationOverlay = new locationOverlay(mMapView);
+
+        //设置定位数据
+        locData = new LocationData();
+        myLocationOverlay.setData(locData);
+        //添加定位图层
+        mMapView.getOverlays().add(myLocationOverlay);
+        myLocationOverlay.enableCompass();
+
+        //修改定位数据后刷新图层生效
+        mMapView.refresh();
+    }
+
+    private void requestLocation(){
+        isRequest = true;
+        mLocClient.requestLocation();
+    }
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListenner implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            Log.i("6174-haha", "onRecieveLoaction");
+            if (location == null){
+                return ;
+            }
+            logLocation(location);
+            setLocData(location);
+            animateToLocData();
+            Log.i(LOGTAG, "on recieveLoation and map refreshed");
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+            if (poiLocation == null){
+                return ;
+            }
+        }
+
+        public void setLocData(BDLocation location){
+            locData.latitude = location.getLatitude();
+            locData.longitude = location.getLongitude();
+            //如果不显示定位精度圈，将accuracy赋值为0即可
+            locData.accuracy = location.getRadius();
+            // 此处可以设置 locData的方向信息, 如果定位 SDK 未返回方向信息，用户可以自己实现罗盘功能添加方向信息。
+            locData.direction = location.getDerect();
+            //更新定位数据
+            myLocationOverlay.setData(locData);
+            //更新图层数据执行刷新后生效
+            mMapView.refresh();
+        }
+
+        public void animateToLocData(){
+            //是手动触发请求或首次定位时，移动到定位点
+            if (isRequest || isNeedLocationAtInitialTime){
+                //移动地图到定位点
+                Log.d("LocationOverlay", "receive location, animate to it");
+                mMapController.animateTo(new GeoPoint((int)(locData.latitude * 1e6), (int)(locData.longitude *  1e6)));
+                isRequest = false;
+                myLocationOverlay.setLocationMode(MyLocationOverlay.LocationMode.FOLLOWING);
+            }
+            //首次定位完成
+            isNeedLocationAtInitialTime = false;
+        }
+
+        public void logLocation(BDLocation location){
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation){
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+            }
+            Log.e(LOGTAG, sb.toString());
+        }
+    }
+
+    //继承MyLocationOverlay重写dispatchTap实现点击处理
+    public class locationOverlay extends MyLocationOverlay{
+
+        public locationOverlay(MapView mapView) {
+            super(mapView);
+            // TODO Auto-generated constructor stub
+        }
+        @Override
+        protected boolean dispatchTap() {
+            // TODO Auto-generated method stub
+            //处理点击事件,弹出泡泡
+//            popupText.setBackgroundResource(R.drawable.popup);
+//            popupText.setText("我的位置");
+//            pop.showPopup(BMapUtil.getBitmapFromView(popupText),
+//                    new GeoPoint((int)(locData.latitude*1e6), (int)(locData.longitude*1e6)),
+//                    8);
+            return true;
+        }
+
+    }
+
+    /**
+     *  MapView的生命周期与Activity同步
+     */
     @Override
     protected void onPause() {
-        /**
-         *  MapView的生命周期与Activity同步，当activity挂起时需调用MapView.onPause()
-         */
         mMapView.onPause();
         super.onPause();
     }
 
     @Override
     protected void onResume() {
-        /**
-         *  MapView的生命周期与Activity同步，当activity恢复时需调用MapView.onResume()
-         */
         mMapView.onResume();
         super.onResume();
     }
 
     @Override
     protected void onDestroy() {
-        /**
-         *  MapView的生命周期与Activity同步，当activity销毁时需调用MapView.destroy()
-         */
+        if (mLocClient != null){
+            mLocClient.stop();
+        }
         mMapView.destroy();
         super.onDestroy();
     }
@@ -210,5 +370,34 @@ class MyGeneralListener implements MKGeneralListener {
 //            Toast.makeText(DemoApplication.getInstance().getApplicationContext(),
 //                    "key认证成功", Toast.LENGTH_LONG).show();
         }
+    }
+}
+
+
+/**
+ * 继承MapView重写onTouchEvent实现泡泡处理操作
+ * @author hejin
+ *
+ */
+class MyLocationMapView extends MapView{
+    static PopupOverlay   pop  = null;//弹出泡泡图层，点击图标使用
+    public MyLocationMapView(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+    public MyLocationMapView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    public MyLocationMapView(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if (!super.onTouchEvent(event)){
+            //消隐泡泡
+            if (pop != null && event.getAction() == MotionEvent.ACTION_UP)
+                pop.hidePop();
+        }
+        return true;
     }
 }
